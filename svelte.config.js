77fb0b7dbe45c08bc4627f4a1d02fc8dbbc4284b
Aflah Mahdi Yazdi <Diyaz.hal22@gmail.com>
2025-05-20 21:04:20 +0700

    Versi 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: '/NAMA_REPO_GITHUB', // kalau deploy ke GitHub Pages
+    }
+  }
+};
